52a173c9e6d3fa151722e819ae4a7094a1718cc9
Aim-ez <154635991+Aim-ez@users.noreply.github.com>
2024-10-08 00:20:57 -0500

    Add boilerplate for application screens

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+
+const authLayout = () => {
+  return (
+    <View>
+      <Text>authLayout</Text>
+    </View>
+  )
+}
+
+export default authLayout
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/reading-list.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+
+const ReadingList = () => {
+  return (
+    <View>
+      <Text>ReadingList</Text>
+    </View>
+  )
+}
+
+export default ReadingList
+
+const styles = StyleSheet.create({})
